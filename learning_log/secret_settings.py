4cb74ad89fcd4255dc1895b576a23e06577d60eb
VinCoD <5895.2020@students.ku.ac.ke>
2023-02-07 11:05:56 +0300

    .railway.app

--- learning_log/secret_settings.py
+++ learning_log/secret_settings.py
@@ -4,4 +4,4 @@ SECRET_KEY = 'django-insecure-%8q=(b_i*=pp^#tu%d#g7zj#*ct1abm-5bozp4^=bv!eh5vaa>
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".railway.app"]
